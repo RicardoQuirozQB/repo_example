7026cbed05b7f9e4ce65c03c6e270ba6a6b0a1a7
Ricardo Quiroz <Ricardo_Quiroz@mckinsey.com>
2022-10-19 13:18:33 -0500

    mock package

---
+++ folder1/pckg/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='so_ds_utils',
+    version='1.0',
+    package=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.6",
+)
